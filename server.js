cfb9cd42962dd64d20daad756edccaef32be8361
wueddyw <wueddyw@gmail.com>
2021-03-31 22:44:59 -0700

    added in new dependencies

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const http = require('http')
+const mysql = require('mysql')
 const app = express()
 const router = express.Router();
 const port = process.env.PORT || 3000
+const dbCon = mysql.createPool({
+    connectionLimit: 10,
+    host: "",
+    user: "",
+    password: "",
+    database: ""
+});
+
 
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
