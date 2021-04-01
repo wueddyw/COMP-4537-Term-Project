21fae8778f7ba2c4fc7e81eae12375268b048764
wueddyw <wueddyw@gmail.com>
2021-03-31 22:38:51 -0700

    added folders

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+const router = express.Router();
 const port = process.env.PORT || 3000
 
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/css', express.static(__dirname + 'public/js'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('Express')
+  res.sendFile(__dirname + '/views/index.html')
 })
 
 app.listen(port, () => {
